805e929a3789eb690e53ae709c6180444b2ac9a1
brava05 <brava_05@mail.ru>
2023-02-12 08:52:14 +0530

    8

---
+++ 567. Permutation in String.py
@@ -0,0 +1,50 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1map = [0]*26
+        s2map = [0]*26
+        for i in range(len(s1)):
+            s1map[ord(s1[i])-ord("a")] += 1
+            s2map[ord(s2[i])-ord("a")] += 1
+
+        count = 0
+        for i in range(26):
+            if s1map[i] == s2map[i]:
+                count += 1
+
+        for i in range(len(s2) - len(s1)):
+            if count == 26:
+                return True
+            right = ord(s2[len(s1)+i])-ord("a")
+            left = ord(s2[i])-ord("a")
+
+            s2map[right] += 1
+            if s2map[right] == s1map[right]:
+                count += 1
+            elif s2map[right] == s1map[right] + 1:
+                count -=1
+
+            s2map[left] -=1
+            if s2map[left] == s1map[left]:
+                count += 1
+            elif s2map[left] == s1map[left] -  1:
+                count -=1
+            print(count)
+
+        
+        return count == 26
+
+
+
+if __name__ == "__main__":
+    s1 = "abv"
+    s2 = "eidbaooo"
+    sol = Solution()
+    
+    s = sol.checkInclusion(s1, s2)
+    print(s)
+# Input: s1 = "ab", s2 = "eidbaooo"
+# Output: true
+# Explanation: s2 contains one permutation of s1 ("ba").
